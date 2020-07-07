4c2f3b89d93bc352a814fde70be4cc3519994b82
jordi temistanjioglus <>
2020-07-07 22:34:33 +0200

    no message

---
+++ exercicis_04_restaurants/src:restaurant/restaurant_1.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+import javax.swing.JOptionPane;
+public class restaurant_1 {
+
+	public static void main(String[] args) {
+
+		int bitllet_cinc=5;
+		int bitllet_deu=10;
+		int bitllet_vint=20;
+		int bitllet_cinquanta=50;
+		int bitllet_cent=100;
+		int bitllet_doscents=200;
+		int bitllet_cinqcents=500;
+		int total=0;
+		
+		String [] nomMenu = new String[5];
+		Integer [] preuPlat = new Integer[5];
+
+	}
+
+}
